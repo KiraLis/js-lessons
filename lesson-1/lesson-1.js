1d41c3039fa929c14ad99b054a94aa92f95e9872
KiraLis <36077391+KiraLis@users.noreply.github.com>
2018-04-01 18:33:35 +0300

    Update lesson-1.js

--- lesson-1/lesson-1.js
+++ lesson-1/lesson-1.js
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Introduction</title>
-  <style>
-  body {
-  background-color: #5d5d5d;
-}
-  p {
-    font-family: Calibri; 
-    color: white;
-  }
-    </style>
-  </head>
-  <body>
-<p style="color: Tomato;">Hello Vylegzhanin!</p>
-<p id="Lesson1"></p>
-<script>
   var person = {
   firstName: "Kira",
   lastName: " Lis.",
@@ -24,6 +6,3 @@
 };
 document.getElementById("Lesson1").innerHTML =
   " My name is " + person.firstName + person.lastName + " I like " + person.preference + " and " + person.preference2 + " >3";
-  </script>
-    </body>
-  </html>
