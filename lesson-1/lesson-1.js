5cadf15a7f2cb04b2d91a5f6062da6f033a33a18
KiraLis <36077391+KiraLis@users.noreply.github.com>
2018-04-01 18:23:29 +0300

    Update lesson-1.js

--- lesson-1/lesson-1.js
+++ lesson-1/lesson-1.js
@@ -1,3 +1,29 @@
-function representPerson(personInfo) {
-    // some your code here...
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Introduction</title>
+  <style>
+  body {
+  background-color: #5d5d5d;
 }
+  p {
+    font-family: Calibri; 
+    color: white;
+  }
+    </style>
+  </head>
+  <body>
+<p style="color: Tomato;">Hello Vylegzhanin!</p>
+<p id="Lesson1"></p>
+<script>
+  var person = {
+  firstName: "Kira",
+  lastName: " Lis.",
+  preference: "mushrooms",
+  preference2: "JavaScript"
+};
+document.getElementById("Lesson1").innerHTML =
+  " My name is " + person.firstName + person.lastName + " I like " + person.preference + " and " + person.preference2 + " >3";
+  </script>
+    </body>
+  </html>
