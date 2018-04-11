45089b52ccc54e839fa0f7bb7364e91a7ea95b04
KiraLis <36077391+KiraLis@users.noreply.github.com>
2018-04-11 12:59:52 +0300

    Update lesson-1.js

--- lesson-1/lesson-1.js
+++ lesson-1/lesson-1.js
@@ -1,3 +1,11 @@
-function representPerson(personInfo) {
-    // some your code here...
+function representPerson() {
+  document.getElementById("lesson-one").innerHTML =
+  " My name is " + person.firstName + person.lastName + " I like " + person.preference + " and " + person.preference2;
 }
+var person = {
+  firstName: "Kira",
+  lastName: " Lis.",
+  preference: "mushrooms",
+  preference2: "JavaScript"
+};
+representPerson();
